08a401a2440f8b25856471788c50f1fdf9b60f09
Olivier_Luethy <olivier@kauz.ch>
2022-04-04 11:51:12 +0200

    Projektdateien hinzufügen.

---
+++ M133+M151-Jubilaeum-der-Musikkapelle-Seetalhorn/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace M133_M151_Jubilaeum_der_Musikkapelle_Seetalhorn.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
