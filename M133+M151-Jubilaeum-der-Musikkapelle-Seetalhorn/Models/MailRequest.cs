b5aac9433d2029e879099524dcb2169ed6ae653e
Olivier_Luethy <olivier@kauz.ch>
2022-04-08 11:32:11 +0200

    Validierung hinzugefügt

---
+++ M133+M151-Jubilaeum-der-Musikkapelle-Seetalhorn/Models/MailRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace M133_M151_Jubilaeum_der_Musikkapelle_Seetalhorn.Models
+{
+    public class MailRequest
+    {
+        public string ToEmail { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public List<IFormFile> Attachments { get; set; }
+    }
+}
--- M133+M151-Jubilaeum-der-Musikkapelle-Seetalhorn/Models/Seetalhorn.cs
+++ M133+M151-Jubilaeum-der-Musikkapelle-Seetalhorn/Models/Seetalhorn.cs
@@ -12,19 +12,19 @@ namespace M133_M151_Jubilaeum_der_Musikkapelle_Seetalhorn.Models
         [MaxLength(200, ErrorMessage = "{0} darf max. 200 Zeichen lang sein")]
         public string Email { get; set; }
         [Display(Name = "Antwort1")]
-        [Required(ErrorMessage = "{0} ist Pflichtfeld")]
+        [Required(ErrorMessage = "Frage 1 ist Pflichtfeld")]
         public string Antwort1 { get; set; }
         [Display(Name = "Antwort2")]
-        [Required(ErrorMessage = "{0} ist Pflichtfeld")]
+        [Required(ErrorMessage = "Frage 2 ist Pflichtfeld")]
         public string Antwort2 { get; set; }
         [Display(Name = "Antwort3")]
-        [Required(ErrorMessage = "{0} ist Pflichtfeld")]
+        [Required(ErrorMessage = "Frage 3 ist Pflichtfeld")]
         public string Antwort3 { get; set; }
         [Display(Name = "Antwort4")]
-        [Required(ErrorMessage = "{0} ist Pflichtfeld")]
+        [Required(ErrorMessage = "Frage 4 ist Pflichtfeld")]
         public string Antwort4 { get; set; }
         [Display(Name = "Antwort5")]
-        [Required(ErrorMessage = "{0} ist Pflichtfeld")]
+        [Required(ErrorMessage = "Frage 5 ist Pflichtfeld")]
         public string Antwort5 { get; set; }
         [Display(Name = "Punkte")]
         [Required(ErrorMessage = "{0} ist Pflichtfeld")]
